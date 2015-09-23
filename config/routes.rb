e1ba386f199b14bc2f92db102f8748284c3ae81c
nagro <nick_agro@hotmail.com>
2015-09-23 15:18:42 -0400

    add contact and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#welcome'
   get 'portfolio' => 'work_examples#examples', as: :examples
+  get 'contact' => 'work_examples#contact', as: :contact
+  get 'about' => 'work_examples#about', as: :about
 end
